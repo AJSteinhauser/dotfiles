ca9113fedcc0979c640424c681bc1569b920622d
AJ Steinhauser <aj.steinhauser@perficient.com>
2024-12-15 10:28:00 -0500

    Dap reconfigured for lazy

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,3 +1,16 @@
+
+local function setDapDefaults()
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#191724' })
+    vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#191724' })
+    vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+
+    local sign = vim.fn.sign_define
+    sign('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
+    sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
+    sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
+    sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
+end
+
 return {
     {
         "rcarriga/nvim-dap-ui",
@@ -8,12 +21,14 @@ return {
             "theHamsta/nvim-dap-virtual-text"
         },
         config = function()
-            local dap = require "dap"
-            local ui = require "dapui"
+            setDapDefaults()
+
+            local dap = require("dap")
+            local ui = require("dapui")
+            local virtualDapText = require("nvim-dap-virtual-text")
 
             ui.setup()
-
-            require("nvim-dap-virtual-text").setup()
+            virtualDapText.setup()
 
             dap.adapters.coreclr = {
                 type = 'executable',
@@ -31,35 +46,6 @@ return {
                     end,
                 },
             }
-            local sign = vim.fn.sign_define
-            vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#31353f' })
-            vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#31353f' })
-            vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
-
-            sign('DapStopped', { text='', texthl='DapStopped', linehl='DapStopped', numhl= 'DapStopped' })
-            sign("DapBreakpoint", { text = "●", texthl = "DapBreakpoint", linehl = "", numhl = ""})
-            sign("DapBreakpointCondition", { text = "●", texthl = "DapBreakpointCondition", linehl = "", numhl = ""})
-            sign("DapLogPoint", { text = "◆", texthl = "DapLogPoint", linehl = "", numhl = ""})
-
-            -- require('dap').defaults.fallback.exception_breakpoints = {'uncaught'}
-            -- dap.defaults.fallback.exception_breakpoints = {'uncaught'}
-            -- dap.set_exception_breakpoints("default")
-
-            vim.keymap.set("n", "<space>b", dap.toggle_breakpoint)
-            vim.keymap.set("n", "<space>gb", dap.run_to_cursor)
-
-            -- Eval var under cursor
-            vim.keymap.set("n", "<space>?", function()
-                require("dapui").eval(nil, { enter = true })
-            end)
-
-            vim.keymap.set("n", "<C-Bslash>", dap.continue)
-            vim.keymap.set("n", "<C-Down>", dap.step_into)
-            vim.keymap.set("n", "<C-Right>", dap.step_over)
-            vim.keymap.set("n", "<C-Up>", dap.step_out)
-            vim.keymap.set("n", "<C-Left>", dap.step_back)
-            vim.keymap.set("n", "<C-F10>", dap.set_exception_breakpoints)
-            vim.keymap.set("n", "<C-F12>", dap.restart)
 
             dap.listeners.before.attach.dapui_config = function()
                 ui.open()
@@ -73,6 +59,79 @@ return {
             dap.listeners.before.event_exited.dapui_config = function()
                 ui.close()
             end
-        end
+        end,
+        keys = {
+            {
+                "<leader>b", function()
+                    local dap = require("dap")
+                    dap.toggle_breakpoint()
+                end,
+                desc = "Toggle breakpoint"
+            },
+            {
+                "<space>gb", function()
+                    local dap = require("dap")
+                    dap.run_to_cursor()
+                end,
+                desc = "Run to cursor"
+            },
+            {
+                "<leader>?", function()
+                    local dap = require("dap")
+                    dap.eval(nil, { enter = true })
+                end,
+                desc = "Evaluate symbol"
+            },
+            {
+                "<C-Bslash>", function()
+                    local dap = require("dap")
+                    dap.continue()
+                end,
+                desc = "Continue"
+            },
+            {
+                "<C-Down>", function()
+                    local dap = require("dap")
+                    dap.step_into()
+                end,
+                desc = "Step into"
+            },
+            {
+                "<C-Right>", function()
+                    local dap = require("dap")
+                    dap.step_over()
+                end,
+                desc = "Step over"
+            },
+            {
+                "<C-Up>", function()
+                    local dap = require("dap")
+                    dap.step_out()
+                end,
+                desc = "Step out"
+            },
+            {
+                "<C-Left>", function()
+                    local dap = require("dap")
+                    dap.step_back()
+                end,
+                desc = "Step back"
+            },
+            {
+                "<C-F10>", function()
+                    local dap = require("dap")
+                    dap.set_exception_breakpoints()
+                end,
+                desc = "Set exceptions breakpoint"
+            },
+            {
+                "<C-F12>", function()
+                    local dap = require("dap")
+                    dap.restart()
+                end,
+                desc = "Restart"
+            },
+        }
+
     }
 }
