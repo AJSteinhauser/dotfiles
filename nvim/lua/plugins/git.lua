0430bc80816fbd92c1a7f4fc22b0e1e7c8c5ee36
AJ Steinhauser <aj.steinhauser@perficient.com>
2024-12-15 10:41:57 -0500

    Added git blamer and minor improvements

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -5,6 +5,21 @@ return {
         config = function()
             require('gitsigns').setup()
         end
+    },
+    {
+        "f-person/git-blame.nvim",
+        event = "VeryLazy",
+        opts = {
+            enabled = false,
+            message_template = " <summary> • <date> • <author> • <<sha>>",
+            date_format = "%m-%d-%Y", -- template for the date, check Date format section for more options
+            virtual_text_column = 1,  -- virtual text start column, check Start virtual text at column section for more options
+        },
+        keys = {
+            {
+                "<leader>i", "<cmd>GitBlameToggle<cr>",desc = "Toggle git blamer"
+            }
+        }
     }
 }
 
